3695030b1993e961625d47d0ab5255e9ea1f7de6
lroberto68 <roberto_luongo@live.com>
2026-07-06 00:34:51 +0200

    Random Password Generator 5

---
+++ index.js
@@ -0,0 +1,32 @@
+const btnEl = document.querySelector(".btn");
+const inputEl = document.getElementById("input");
+const copyIconEl = document.querySelector(".fa-copy");
+const alertContainerEl = document.querySelector(".alert-container");
+
+btnEl.addEventListener("click", ()=>{
+    createPassword();
+})
+
+copyIconEl.addEventListener("click", ()=>{
+    copyPassword();
+})
+
+function createPassword(){
+    const chars = "0123456789abcdefghijklmnopqrstuvwxtz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const passwordLenght = 14;
+
+    let password = "";
+    for(let i = 0; i < passwordLenght; i++){
+        const randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+        console.log(randomNumber, password);
+    }
+    inputEl.value = password;
+    alertContainerEl.innerText = password + " copied!";
+}
+
+function copyPassword(){
+    inputEl.select();
+    inputEl.setSelectionRange(0,9999);
+    navigator.clipboard.writeText(inputEl.value);
+}
